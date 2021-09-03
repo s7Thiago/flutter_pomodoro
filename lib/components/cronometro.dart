b7a71f645d4c68b644dde68ff19e5d7c66281cbe
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-02 22:35:20 -0300

    Chronemeter button component

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/cronometro_botao.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({Key? key}) : super(key: key);
@@ -9,22 +10,49 @@ class Cronometro extends StatelessWidget {
       color: Colors.red,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
+        children: [
+          const Text(
             'Hora de Trabalhar',
             style: TextStyle(
               fontSize: 40,
               color: Colors.white,
             ),
           ),
-          SizedBox(height: 20),
-          Text(
+          const SizedBox(height: 20),
+          const Text(
             '25:00',
             style: TextStyle(
               fontSize: 120,
               color: Colors.white,
             ),
           ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: CronometroBotao(
+                  label: 'Iniciar',
+                  icon: Icons.play_arrow,
+                ),
+              ),
+              // Padding(
+              //   padding: EdgeInsets.all(10),
+              //   child: CronometroBotao(
+              //     label: 'Parar',
+              //     icon: Icons.stop,
+              //   ),
+              // ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: CronometroBotao(
+                  label: 'Reiniciar',
+                  icon: Icons.refresh,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
